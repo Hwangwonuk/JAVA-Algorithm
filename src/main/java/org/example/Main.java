fab671d435a9ecc6100123b0110781560fbe22f3
Hwangwonuk <hok0173@naver.com>
2023-01-14 19:46:34 +0900

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+/**
+ * create on ${DATE}. create by ${PRODUCT_NAME}.
+ *
+ * <p> </p>
+ * <p> {@link } and {@link } </p> *
+ *
+ * @author wonukHwang
+ * @version 1.0
+ * @see
+ * @since (ex : 5 + 5)
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
