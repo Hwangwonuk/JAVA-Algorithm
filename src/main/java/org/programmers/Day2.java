9b39911632963777ae159138cfd60379ab2d7fa3
Hwangwonuk <hok0173@naver.com>
2023-02-23 21:36:19 +0900

    분수의 덧셈

--- src/main/java/org/programmers/Day2.java
+++ src/main/java/org/programmers/Day2.java
@@ -65,4 +65,40 @@ public class Day2 {
     }
     return answer;
   }
+
+  /**
+   * 분수의 덧셈
+   * 문제 설명
+   * 첫 번째 분수의 분자와 분모를 뜻하는 numer1, denom1,
+   * 두 번째 분수의 분자와 분모를 뜻하는 numer2, denom2가 매개변수로 주어집니다.
+   * 두 분수를 더한 값을 기약 분수로 나타냈을 때
+   * 분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+   *
+   * 제한사항
+   * 0 < numer1, denom1, numer2, denom2 < 1,000
+   *
+   * 입출력 예
+   * numer1	denom1	numer2	denom2	result
+   *   1	    2	      3	      4	    [5, 4]
+   *   9	    2	      1	      3	    [29, 6]
+   */
+  public int gcd(int num1, int num2) {
+    if (num2 == 0) {
+      return num1;
+    }
+    return gcd(num2, num1 % num2);
+  }
+
+  public int[] solution3(int numer1, int denom1, int numer2, int denom2) {
+
+    numer1 *= denom2;
+    numer2 *= denom1;
+    int[] answer = new int[]{numer1 + numer2, denom1 * denom2};
+    int max = gcd(answer[0], answer[1]);
+
+    answer[0] /= max;
+    answer[1] /= max;
+
+    return answer;
+  }
 }
