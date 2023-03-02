b50ef3b02a6adef84f47c60b0da2d62c4b8f777b
Hwangwonuk <hok0173@naver.com>
2023-03-02 09:22:08 +0900

    최빈값 구하기

--- src/main/java/org/programmers/Day3.java
+++ src/main/java/org/programmers/Day3.java
@@ -99,4 +99,60 @@ public class Day3 {
 
     return answer;
   }
+
+  /**
+   * 최빈값 구하기
+   *
+   * 문제 설명
+   * 최빈값은 주어진 값 중에서 가장 자주 나오는 값을 의미합니다.
+   * 정수 배열 array가 매개변수로 주어질 때, 최빈값을 return 하도록 solution 함수를 완성해보세요.
+   * 최빈값이 여러 개면 -1을 return 합니다.
+   *
+   * 제한사항
+   * 0 < array의 길이 < 100
+   * 0 ≤ array의 원소 < 1000
+   *
+   * 입출력 예
+   *     array	               result
+   * [1, 2, 3, 3, 3, 4]	         3
+   * [1, 1, 2, 2]	              -1
+   *      [1]	                   1
+   *
+   * 입출력 예 설명
+   * 입출력 예 #1
+   * [1, 2, 3, 3, 3, 4]에서 1은 1개 2는 1개 3은 3개 4는 1개로 최빈값은 3입니다.
+   *
+   * 입출력 예 #2
+   * [1, 1, 2, 2]에서 1은 2개 2는 2개로 최빈값이 1, 2입니다. 최빈값이 여러 개이므로 -1을 return 합니다.
+   *
+   * 입출력 예 #3
+   * [1]에는 1만 있으므로 최빈값은 1입니다.
+   */
+  public int solution3(int[] array) {
+    int answer = array[0]; // 첫번째 값으로 초기화
+    int max = 0; // max값 초기화
+    int frequent[] = new int[1000]; // 제한사항 배열 크기 초기화
+
+    for(int i = 0; i < array.length; i++) {
+      frequent[array[i]]++; // 입력받은 0부터 length까지 반복문. 해당 인덱스로 ++1
+
+      if(max < frequent[array[i]]) { // max값이 frequent의 인덱스 값보다 크면
+        max = frequent[array[i]]; // max = frequent
+        answer = array[i]; // 답 = array[i]
+      }
+    }
+
+    // 중복검사
+    int temp = 0;
+    for(int j = 0; j < 1000; j++) {
+      if(max == frequent[j]) { // max값과 같은 frequent가 있다면
+        temp++; // temp ++
+      }
+      if(temp > 1)  { // 중복된 값이 존재한다면
+        answer = -1; // 답 = -1
+      }
+    }
+
+    return answer;
+  }
 }
