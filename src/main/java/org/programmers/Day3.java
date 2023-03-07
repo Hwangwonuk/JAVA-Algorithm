dbddde681a06933ec317520c5f861154a02411c0
Hwangwonuk <hok0173@naver.com>
2023-03-07 10:16:54 +0900

    짝수는 싫어요

--- src/main/java/org/programmers/Day3.java
+++ src/main/java/org/programmers/Day3.java
@@ -148,4 +148,44 @@ public class Day3 {
     return answer;
   }
 
+  /**
+   * 짝수는 싫어요
+   * 문제 설명
+   * 정수 n이 매개변수로 주어질 때, n 이하의 홀수가 오름차순으로 담긴 배열을 return하도록 solution 함수를 완성해주세요.
+   *
+   * 제한사항
+   * 1 ≤ n ≤ 100
+   *
+   * 입출력 예
+   *   n	            result
+   *  10	       [1, 3, 5, 7, 9]
+   *  15	 [1, 3, 5, 7, 9, 11, 13, 15]
+   *
+   * 입출력 예 설명
+   *
+   * 입출력 #1
+   * 10 이하의 홀수가 담긴 배열 [1, 3, 5, 7, 9]를 return합니다.
+   *
+   * 입출력 #1
+   * 15 이하의 홀수가 담긴 배열 [1, 3, 5, 7, 9, 11, 13, 15]를 return합니다.
+   */
+  public int[] solution5(int n) {
+    int[] answer;
+    int num = 0;
+
+    if (n % 2 == 0) { // 2로 나눈 나머지가 0이면 == 짝수이면
+      answer = new int[n / 2];
+    } else { // 홀수이면
+      answer = new int[n / 2 + 1];
+    }
+
+    for (int i = 0; i <= n; i++) {
+      if (i % 2 == 1) {
+        answer[num] = i;
+        num++;
+      }
+    }
+    return answer;
+  }
+
 }
