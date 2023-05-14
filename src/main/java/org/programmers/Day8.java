9757d23a119479fa408b633f69429521a3e45a50
Hwangwonuk <hok0173@naver.com>
2023-05-15 00:28:47 +0900

    외계행성의 나이 다른풀이

--- src/main/java/org/programmers/Day8.java
+++ src/main/java/org/programmers/Day8.java
@@ -110,4 +110,16 @@ public class Day8 {
     return answer;
   }
 
+  public String solution2(int age) {
+    String answer = "";
+    String[] alpha = new String[] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+
+    while(age > 0){
+      answer = alpha[age % 10] + answer;
+      age /= 10;
+    }
+
+    return answer;
+  }
+
 }
